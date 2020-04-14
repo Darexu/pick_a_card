ae9b8ac34524654704946dba94bd4cff706a1adb
darexu <darexr.u@mail.ru>
2020-04-14 03:05:42 +0300

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J','O', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+values = %w[2 3 4 5 6 7 8 9 10 J O K A]
+suits = %w[♦ ♠ ♥ ♣]
 
 puts "#{values.sample} of #{suits.sample}"
