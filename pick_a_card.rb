154984a6bfcf0a4a5067b9e25ca61f5697c4b833
darexu <darexr.u@mail.ru>
2020-06-09 01:55:05 +0300

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,19 @@
 values = %w[2 3 4 5 6 7 8 9 10 J O K A]
 suits = %w[♦ ♠ ♥ ♣]
 
-puts "#{values.sample} of #{suits.sample}"
+cards = []
+
+values.each do |value|
+  suits.each do |suit|
+    cards << "#{value} of #{suit}"
+  end
+end
+
+cards.shuffle!
+
+puts "Сколько карт нужно?"
+number = STDIN.gets.to_i
+
+number.times do
+  puts cards.pop
+end
